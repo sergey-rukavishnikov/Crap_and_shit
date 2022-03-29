0227e577e8ddb1f4cfb0722ca73d0d6eaedb019f
SergeyRukavishnikov <rukavishnikov.s@gubkins.onmicrosoft.com>
2022-03-29 18:58:50 +0300

    last

---
+++ cofe-auto/cofe-auto/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        enum size
+        {
+            маленькая,
+            средняя,
+            большая
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Выберите порция {(int)size.маленькая} = {size.маленькая}, {(int)size.средняя} = {size.средняя}, {(int)size.большая} = {size.большая}");
+        }
+        int.TryParse(Console.ReadLine(), out int vote);
+        switch(vote):
+            case(0):
+            Console.Wri
+    }
+}
