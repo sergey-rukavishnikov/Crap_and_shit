00625887cc6347f1e2b04b099375d9d77500c1b8
SergeyRukavishnikov <rukavishnikov.s@gubkins.onmicrosoft.com>
2022-03-29 18:42:42 +0300

    last_change

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        enum game
+        {
+            камень,
+            бумага,
+            ножницы
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Выберите предмет {(int) game.камень} = {game.камень}, {(int)game.бумага} = {game.бумага}, {(int)game.ножницы} = {game.ножницы}");
+            int.TryParse(Console.ReadLine(), out int player1);
+            Console.Clear();
+            Console.WriteLine($"Выберите предмет {(int)game.камень} = {game.камень}, {(int)game.бумага} = {game.бумага}, {(int)game.ножницы} = {game.ножницы}");
+            int.TryParse(Console.ReadLine(), out int player2);
+            if (player1 > player2)
+            {
+                if((player1 - player2) > 1)
+                    Console.WriteLine("Выйграл второй игрок");
+                else
+                Console.WriteLine("Выйграл первый игрок");
+            }
+            else
+            {
+                if (player2 == player1)
+                {
+                    Console.WriteLine("Ничья!");
+                }
+                else
+                Console.WriteLine("Выйграл второй игрок");
+            }
+            
+        }
+    }
+}
---
+++ threechar/threechar/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("number:");
+int.TryParse(Console.ReadLine(), out int num);
+if ((num / 100) % 10 != 0)
+{
+    if((num / 1000) % 10 != 0 )
+    {
+        Console.WriteLine("нет");
+    }
+    else
+    Console.WriteLine("трёхзначное");
+}
+else
+{
+    Console.WriteLine("нет");
+}
