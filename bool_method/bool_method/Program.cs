241538d5bb3903492af04b44501c897946df55d3
SergeyRukavishnikov <rukavishnikov.s@gubkins.onmicrosoft.com>
2022-04-12 17:58:57 +0300

    12.04.2022(1)

---
+++ bool_method/bool_method/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static bool boolParse()
+        {
+            string input = Console.ReadLine();
+            while(input != "0" && input != "1")
+            {
+                Console.WriteLine("Введите зановов: ");
+                input = Console.ReadLine();
+            }
+            return input == "1";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввод: ");
+            bool var = boolParse();
+            if (var)
+            {
+                Console.WriteLine("Да");
+            }
+            else
+            {
+                Console.WriteLine("Нет");
+            }
+        }
+    }
+}
